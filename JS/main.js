2571c5b0574ad24f3a5e2e871747b5b13224ad5d
Anandhbasha <anadhakm@gmail.com>
2026-02-11 20:36:40 +0530

    js

---
+++ JS/main.js
@@ -0,0 +1,22 @@
+// Hoisting
+// variable declaration
+// var
+// console.log(a); //undefined ->datatype
+
+var a=10
+var a =50 //redeclaration
+a=60 //re assign
+// console.log("hello",a)
+
+//let
+// console.log(b);
+// let b=52
+//unable to redeclaration
+// let b
+// b=88
+// console.log(b);
+
+//const
+const c =10
+c=66
+console.log(c);
