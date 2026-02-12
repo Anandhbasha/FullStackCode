567e1cf75e08b4d1396faa6d21ea6b1b1b1b1fa0
Anandhbasha <anadhakm@gmail.com>
2026-02-12 20:32:39 +0530

    operator

--- JS/main.js
+++ JS/main.js
@@ -3,9 +3,9 @@
 // var
 // console.log(a); //undefined ->datatype
 
-var a=10
-var a =50 //redeclaration
-a=60 //re assign
+// var a=10
+// var a =50 //redeclaration
+// a=60 //re assign
 // console.log("hello",a)
 
 //let
@@ -17,6 +17,42 @@ a=60 //re assign
 // console.log(b);
 
 //const
-const c =10
-c=66
-console.log(c);
+// const c =10
+// c=66
+// console.log(c);
+
+// variable scope 
+// var
+// global scope
+//let const
+//local scope
+
+// {
+//     let c =60
+//     console.log(c);
+// }
+
+
+// varibale not start with number
+//dont use Special Characters
+// keyword dont use
+// meaning full Names
+
+// Operators
+//Arithmetic
+// +,-,*,/,%,++--
+let a =10
+let b=5
+
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+console.log(a++);
+console.log(++a); //11 12
+console.log(--a); //11
+console.log(a--); //11
+//10
+console.log(a);
+
