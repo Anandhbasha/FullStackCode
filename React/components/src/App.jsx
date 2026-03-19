f5449d826735fce6787cf8c058929665f6e9d530
Anandhbasha <anadhakm@gmail.com>
2026-03-19 19:46:20 +0530

    components

---
+++ React/components/src/App.jsx
@@ -0,0 +1,27 @@
+import Button from "./Button"
+
+const App = ()=>{
+    return (
+      <div>
+        <h1>Hello Welcome</h1>
+        <h2>This Is React</h2>
+        <Button />
+        <Button />
+        <Button />
+        <Button />
+        <Button />
+        <Button />
+        <Button />
+        <Button />
+        <Button />
+      </div>
+    )
+}
+
+export default App
+
+// first must be a captial letter
+// components name is function name
+// export
+// return
+// one can return only one element ath the time 
