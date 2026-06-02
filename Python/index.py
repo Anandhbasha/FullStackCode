606e2f6fc793f2b60cb126f3c19dd450adaa0e91
Anandhbasha <anadhakm@gmail.com>
2026-06-02 21:17:22 +0530

    py

---
+++ Python/index.py
@@ -0,0 +1,58 @@
+# # print("Hello, World!","Welcome to Python programming.",sep="/")
+
+# a=10
+# b=20  
+
+# name = "bala"
+
+# # datatype
+# # int
+# print(type(a))
+# # float
+# print(type(3.14))
+# # string
+# print(type(name))
+# # boolean
+# print(type(True))
+
+# # complex
+# # list
+# list1 = [1,2,3,4,5]
+# print(list1[0])
+# list1[0] = 10
+# print(list1)
+# #tuple  
+# # t = (1,2,3,4,5)
+# # # t[1] = 10
+# # print(t[1])
+# # # set   
+# # s = {1,2,3,4,5,5,1,3,1}
+# # print(s)
+# # # dictionary
+# # person = {"name":"bala","age":30,"city":"chennai"}
+# # print(person["name"])
+
+# userName = input("Enter your name: ")
+# # slicing Method
+# # staring value:stop value:step value
+# print(userName[:3:-1])
+
+
+# # memory allogation
+# a = [1,2,3,4]
+# b = [1,2,3,4]
+
+# x=10
+# y=10
+
+# # membership
+# print(a is not b)
+# print(x is y)
+
+# # identidy 
+# list1 = [20,30,40]
+# print(20 in list1)
+
+# tenary
+age=18
+print(age>=18 if "Eligible" else "not Eligible")
