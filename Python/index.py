833fb50421c63bbe71e05e5a7c9fc939c498ae7d
Anandhakumar <anandhaanavamani@gmail.com>
2026-06-16 21:35:19 +0530

    oops

--- Python/index.py
+++ Python/index.py
@@ -1,159 +1,181 @@
-# # print("Hello, World!","Welcome to Python programming.",sep="/")
+# # # print("Hello, World!","Welcome to Python programming.",sep="/")
 
-# a=10
-# b=20  
+# # a=10
+# # b=20  
 
-# name = "bala"
+# # name = "bala"
 
-# # datatype
-# # int
-# print(type(a))
-# # float
-# print(type(3.14))
-# # string
-# print(type(name))
-# # boolean
-# print(type(True))
+# # # datatype
+# # # int
+# # print(type(a))
+# # # float
+# # print(type(3.14))
+# # # string
+# # print(type(name))
+# # # boolean
+# # print(type(True))
 
-# # complex
-# # list
-# list1 = [1,2,3,4,5]
-# print(list1[0])
-# list1[0] = 10
-# print(list1)
-# #tuple  
-# # t = (1,2,3,4,5)
-# # # t[1] = 10
-# # print(t[1])
-# # # set   
-# # s = {1,2,3,4,5,5,1,3,1}
-# # print(s)
-# # # dictionary
-# # person = {"name":"bala","age":30,"city":"chennai"}
-# # print(person["name"])
+# # # complex
+# # # list
+# # list1 = [1,2,3,4,5]
+# # print(list1[0])
+# # list1[0] = 10
+# # print(list1)
+# # #tuple  
+# # # t = (1,2,3,4,5)
+# # # # t[1] = 10
+# # # print(t[1])
+# # # # set   
+# # # s = {1,2,3,4,5,5,1,3,1}
+# # # print(s)
+# # # # dictionary
+# # # person = {"name":"bala","age":30,"city":"chennai"}
+# # # print(person["name"])
 
-# userName = input("Enter your name: ")
-# # slicing Method
-# # staring value:stop value:step value
-# print(userName[:3:-1])
+# # userName = input("Enter your name: ")
+# # # slicing Method
+# # # staring value:stop value:step value
+# # print(userName[:3:-1])
 
 
-# # memory allogation
-# a = [1,2,3,4]
-# b = [1,2,3,4]
+# # # memory allogation
+# # a = [1,2,3,4]
+# # b = [1,2,3,4]
 
-# x=10
-# y=10
+# # x=10
+# # y=10
 
-# # membership
-# print(a is not b)
-# print(x is y)
+# # # membership
+# # print(a is not b)
+# # print(x is y)
 
-# # identidy 
-# list1 = [20,30,40]
-# print(20 in list1)
+# # # identidy 
+# # list1 = [20,30,40]
+# # print(20 in list1)
 
-# tenary
-# age=18
-# print(age>=18 if "Eligible" else "not Eligible")
+# # tenary
+# # age=18
+# # print(age>=18 if "Eligible" else "not Eligible")
 
-# conditional statments
-# if
-# if else
-# elif
+# # conditional statments
+# # if
+# # if else
+# # elif
 
-# if condtiion:
+# # if condtiion:
+# #     # block
+
+# # temp = int(input("Enter the Temperature:"))
+
+# # if temp >32:
+# #     print("Switch on the AC")
+# # elif temp<20:
+# #     print("Switch off the AC and Fan")
+# # else:
+# #     print("Switch on the Fan")
+
+
+# # looping statement
+# # for
+# # for i in value:
 #     # block
 
-# temp = int(input("Enter the Temperature:"))
+# # for i in range(5):
+# #     print(i)
 
-# if temp >32:
-#     print("Switch on the AC")
-# elif temp<20:
-#     print("Switch off the AC and Fan")
-# else:
-#     print("Switch on the Fan")
+# # 0
+# # 1
+# # 2
+# # 3
+# # 4
+# # for i in range(1,5):
+# #     print(i)
 
+# # for i in range(1,10,3):
+# #     print(i)
 
-# looping statement
-# for
-# for i in value:
-    # block
+# # list = ["apple","orange","banana","kiwi"]
+# # # for i in range(0,len(list)):
+# # #     print(list[i])
 
-# for i in range(5):
-#     print(i)
-
-# 0
-# 1
-# 2
-# 3
-# 4
-# for i in range(1,5):
-#     print(i)
-
-# for i in range(1,10,3):
-#     print(i)
-
-# list = ["apple","orange","banana","kiwi"]
-# # for i in range(0,len(list)):
-# #     print(list[i])
-
-# for i in list:
-#     print(i)
+# # for i in list:
+# #     print(i)
 
 
-#   *
-#  ***
-# *****
-#*******
+# #   *
+# #  ***
+# # *****
+# #*******
 
-# row = int(input("Enter thennumber of Rows:"))
+# # row = int(input("Enter thennumber of Rows:"))
 
-# for i in range(1,row):
-#     print(" "*(row-i)+"*"*(2*i-1))
+# # for i in range(1,row):
+# #     print(" "*(row-i)+"*"*(2*i-1))
 
-#     *
-#    ***
-#   *****
-#  *******
-# # while
-# while condition:
+# #     *
+# #    ***
+# #   *****
+# #  *******
+# # # while
+# # while condition:
 
-# oops
-# car1Name = "swift"
-# car1Color = "white"
-# car1NoofWheels = 4
-# car1NofMirror = 2
+# # oops
+# # car1Name = "swift"
+# # car1Color = "white"
+# # car1NoofWheels = 4
+# # car1NofMirror = 2
 
-# def acc():
-#     print("Car1 is moving")
-# def breaks():
-#     print("Car1 is Stopped")
-
-
-# car2Name = "swift"
-# car2Color = "red"
-# car2NoofWheels = 4
-# car2NofMirror = 2
-
-# def acc():
-#     print("Car2 is moving")
-# def breaks():
-#     print("Car2 is Stopped")
+# # def acc():
+# #     print("Car1 is moving")
+# # def breaks():
+# #     print("Car1 is Stopped")
 
 
-class Car:
-    carName = "swift"
-    carColor = "red"
-    carNoofWheels = 4
-    carNofMirror = 2
+# # car2Name = "swift"
+# # car2Color = "red"
+# # car2NoofWheels = 4
+# # car2NofMirror = 2
 
-    def acc():
-        print("Car is moving")
-    def breaks():
-        print("Car is Stopped")
+# # def acc():
+# #     print("Car2 is moving")
+# # def breaks():
+# #     print("Car2 is Stopped")
 
-c1 = Car()
-print(c1.carColor)
-c1.carColor = "white"
-print(c1.carColor)
+
+# class Car:
+#     carName = "swift"
+#     carColor = "red"
+#     carNoofWheels = 4
+#     carNofMirror = 2
+
+#     def acc():
+#         print("Car is moving")
+#     def breaks():
+#         print("Car is Stopped")
+
+# c1 = Car()
+# print(c1.carColor)
+# c1.carColor = "white"
+# print(c1.carColor)
+
+# def add(a,b):
+#     print(a+b)
+
+# add(10,20)
+
+# def addition(*args):
+#     for i in args:
+#         return i
+
+
+# print(addition(10,20,30,40,50))
+# def greet():
+#     print("Hello Welcome")
+
+# greet()
+# addition(10,20,30,40,50)
+# addition(10,20,30,40)
+# addition(10,20,30)
+# addition(10,20)
+
+
---
+++ Python/oop.py
@@ -0,0 +1,86 @@
+# # class
+# # object
+
+# class Fan:
+#     # propeties
+#     fanColor = "brown"
+#     fanSpeed = "1200rpm"
+#     noofWings = 3
+#     # methods
+#     def rotate(self):
+#         print("Fan is running")
+# # instence or Object
+# f1 = Fan()
+# f1.fanSpeed = "1000rpm"
+# print(f1.fanColor)
+# print(f1.fanSpeed)
+# print(f1.noofWings)
+# f1.rotate()
+
+# f2 = Fan()
+# f2.fanColor = "blue"
+# print(f2.fanColor)
+# print(f2.fanSpeed)
+# print(f2.noofWings)
+# f2.rotate()
+
+
+
+
+# static Method
+# class Fan:   
+#     fanColor = "brown"
+#     fanSpeed = "1200rpm"
+#     noofWings = 3
+#     # methods
+#     def rotate():
+#         print("Fan is running")
+# # instence or Object
+# Fan.fanColor = "Green"
+# print(Fan.fanColor)
+
+# # class method
+# class Fan:   
+#     fanColor = "brown"
+#     fanSpeed = "1200rpm"
+#     noofWings = 3
+#     # methods
+#     def rotate(cls):
+#         print("Fan is running")
+# # instence or Object
+# f1= Fan()
+# f1.fanColor = "Green"
+# print(Fan.fanColor)
+
+
+
+
+# class Car:
+#     def __init__(self):
+#         carName = "BMW"
+#         print("Hello This Constructor function")
+#         print("Car Name is:",carName)
+
+# c =Car()
+
+# Access Specifiers
+    # scope of the variable
+        #private
+        #protected
+        #public
+# Encapsulation
+# Inheritence
+    # single Inheritence
+    # mutiple Inheritence
+    # multilevel Inheritence
+    # Hyrachical Inheritence
+    # Hybrid Inheritence
+# Abstraction
+# polymorphism
+    #method overloading
+    # method overridding
+
+# Operator overloading
+# composition
+
+# exception handling
