4eea77f09e1a2d760911edadb653fca5589de7b9
Anandhakumar <anandhaanavamani@gmail.com>
2026-06-18 21:32:42 +0530

    inheritence

--- Python/oop.py
+++ Python/oop.py
@@ -25,8 +25,6 @@
 # f2.rotate()
 
 
-
-
 # static Method
 # class Fan:   
 #     fanColor = "brown"
@@ -66,21 +64,133 @@
 # Access Specifiers
     # scope of the variable
         #private
+# class BankAccount:
+#     __balance = 500
+#     def showBalane(self):
+#         print(self.__balance)
+
+# acc =  BankAccount()
+# acc.showBalane()
         #protected
         #public
-# Encapsulation
-# Inheritence
-    # single Inheritence
-    # mutiple Inheritence
-    # multilevel Inheritence
-    # Hyrachical Inheritence
-    # Hybrid Inheritence
-# Abstraction
-# polymorphism
-    #method overloading
-    # method overridding
+# # Encapsulation
+# class BankAccount:
+#     __balance = 500   
+#     def deposit(self,amount):
+#         self.__balance += amount
+#     def withdrawn(self,amount):
+#         if amount<self.__balance:
+#             self.__balance -= amount
+#         else:
+#             print("Insufficient Balance")
+#     def showBalane(self):
+#         return self.__balance
+# acc =  BankAccount()
+# print(acc.showBalane())
+# acc.deposit(10000)
+# print(acc.showBalane())
+# acc.withdrawn(1000)
+# print(acc.showBalane())
 
+
+# Inheritence
+#     # single Inheritence
+# class Jap:
+#     courseTitle1 = "Python"
+#     courseTitle2 = "React"
+#     courseTitle3 = "Node"
+#     courseTitle4 = "MongoDb"
+#     courseTitle5 = "SQL"
+#     courseTitle6 = "JS"
+#     courseTitle7 = "CSS"
+#     courseTitle8 = "HTML"
+# class Mithun(Jap):
+#     pass
+
+# m = Mithun()
+# print(m.courseTitle1)
+# print(m.courseTitle2)
+# print(m.courseTitle3)
+    # mutiple Inheritence
+# class Singer:
+#     def show(self):
+#         print("He is a Singer")
+# class Dancer:
+#     def show(self):
+#         print("He is a Dancer")
+
+# class Person(Dancer,Singer):
+#     pass
+
+
+# P = Person()
+# P.show()
+    # multilevel Inheritence
+# class Qtree:
+#     trainer = "Anandh"
+# class AssignTrainer(Qtree):
+#     pass
+# class Students(AssignTrainer):
+#     pass
+
+# S= Students()
+# print(S.trainer)
+
+    # Hyrachical Inheritence
+# class Jap:
+#     courseTitle1 = "Python"
+#     courseTitle2 = "React"
+#     courseTitle3 = "Node"
+#     courseTitle4 = "MongoDb"
+#     courseTitle5 = "SQL"
+#     courseTitle6 = "JS"
+#     courseTitle7 = "CSS"
+#     courseTitle8 = "HTML"
+# class Mithun(Jap):
+#     pass
+# class Jeeve(Jap):
+#     pass
+
+    # Hybrid Inheritence
+# class Qtree:
+#     course1 = "Jap"
+#     course1 = "Python"
+#     course1 = "Java"
+#     course1 = "MERN"
+# class Trainer(Qtree):
+#     name = "Anandh"
+# class Mithun(Trainer):
+#     pass
+# class Jeeve(Trainer):
+#     pass
+
+
+# polymorphism
+#     #method overloading
+# class Add:
+#     def Addition(self,a=0,b=0,c=0):
+#         print(a+b+c)
+# a = Add()
+# a.Addition(10)
+# a.Addition(10,20)
+# a.Addition(10,20,300)
+    # method overridding
+# class Singer:
+#     def show(self):
+#         print("He is a Singer")
+# class Dancer:
+#     def show(self):
+#         print("He is a Dancer")
+
+# class Person(Dancer,Singer):
+#     pass
+
+# P = Person()
+# P.show()
+# S =Singer()
+# S.show()
+
+# Abstraction
 # Operator overloading
 # composition
-
 # exception handling
