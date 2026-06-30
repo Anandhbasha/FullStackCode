9a1d54fa1a1f46f3e575d48757bdbed11e6e4f65
Anandhakumar <anandhaanavamani@gmail.com>
2026-06-30 21:41:23 +0530

    apis

--- Python/Flask/Controller/controller.py
+++ Python/Flask/Controller/controller.py
@@ -1,8 +1,15 @@
-def read():
-    return "Hello Flask"
-def about():
-    return "About Page"
-def ser():
-    return "Services Page"
-def con():
-    return "Contact Page"
+from flask import request,jsonify
+from config.db import collection
+from bson import ObjectId
+# Add student
+def addStudent():
+    data = request.get_json()
+    res = collection.insert_one(data)
+    return jsonify({"message":"User added Sucessfully","id":str(res.inserted_id)})
+# read
+def getStudent():
+    students = []
+    for student in collection.find():
+        student["_id"] = str(student["_id"])
+        students.append(student)
+        return jsonify(students)
--- Python/Flask/Router/router.py
+++ Python/Flask/Router/router.py
@@ -1,9 +1,11 @@
 from flask import Blueprint
-from Controller.controller import read,about,ser,con
-
+from Controller.controller import addStudent,getStudent
 router = Blueprint("crud",__name__)
 
-router.route("/")(read)
-router.route("/about")(about)
-router.route("/services")(ser)
-router.route("/contact")(con)
+# router.route("/")(read)
+# router.route("/about")(about)
+# router.route("/services")(ser)
+# router.route("/contact")(con)
+
+router.route("/add",methods =["POST"])(addStudent)
+router.route("/show",methods =["GET"])(getStudent)
